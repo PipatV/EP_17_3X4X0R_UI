33228928ca476fff1ba16a0bce6f065f604ba585
Pack <packaq3@gmail.com>
2022-09-08 13:17:05 +0700

    Initial Commit

---
+++ X4X0R_UI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  X4X0R_UI
+//
+//  Created by Pipat on 8/9/2565 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ X4X0R_UI/X4X0R_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  X4X0R_UIApp.swift
+//  X4X0R_UI
+//
+//  Created by Pipat on 8/9/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct X4X0R_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
